79d30c7e184862b30e45c463c0ab9d89b2e2d912
KarthigaBoopathi <111348753+KarthigaBoopathi@users.noreply.github.com>
2022-08-18 11:37:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float radius;
+            float pi = 3.14f;
+            Console.WriteLine("enter radius");
+            radius = Convert.ToSingle(Console.ReadLine());
+            double area = pi * radius * radius;
+            Console.WriteLine(area);
+            Console.ReadLine();
+           
+        }
+    }
+}
