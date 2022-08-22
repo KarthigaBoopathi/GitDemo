3baa7968c7fec365a902af050cc767ff0d8ec77c
KarthigaBoopathi <111348753+KarthigaBoopathi@users.noreply.github.com>
2022-08-22 22:32:14 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,315 +1,339 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment_day4
+namespace Assignment_day5
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //q1
+        /*  int i;
+          int[] arr = new int[10];
+          Console.WriteLine("Input 10 elements in the array :\n");
+          for (i = 0; i < 10; i++)
+          {
+              Console.WriteLine("element - {0} : ", i);
+              arr[i] = Convert.ToInt32(Console.ReadLine());
+          }
+
+          Console.WriteLine("\nElements in array are: ");
+          for (i = 0; i < 10; i++)
+          {
+              Console.WriteLine("{0}  ", arr[i]);
+          }
+          Console.WriteLine("\n");
+          Console.ReadLine(); */
+
+        //q2
+        /* int i, n;
+         int[] a = new int[3];
+         Console.WriteLine("Input the number of elements to store in the array :");
+         n = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Input {0} array elements:\n", n);
+         for (i = 0; i < n; i++)
+         {
+             Console.WriteLine("element - {0} : ", i);
+             a[i] = Convert.ToInt32(Console.ReadLine());
+         }
+         Console.WriteLine("\n Values into the array are : \n");
+         for (i = 0; i < n; i++)
+         {
+             Console.WriteLine("{0}  ", a[i]);
+         }
+         Console.WriteLine("\n\n Array in Reverse order are :\n");
+         for (i = n - 1; i >= 0; i--)
+         {
+             Console.WriteLine("{0} ", a[i]);
+         }
+         Console.WriteLine("\n\n");
+         Console.ReadLine(); */
+
+
+        //q3
+        /* int[] a = new int[3];
+        int i, n, sum = 0;
+        Console.WriteLine("Input the number of elements to be stored in the array :");
+        n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter {0} elements in the array :\n", n);
+        for (i = 0; i < n; i++)
         {
-            //q1 
-            /*int a, b, minimum, maximum;
-            Console.Write("Enter the value of a: ");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the value of b: ");
-            b = Convert.ToInt32(Console.ReadLine());
-            if (a > b)
-                maximum = a;
-            else
-                maximum = b;
-            if (a < b)
-                minimum = a;
-            else
-                minimum = b;
-            Console.WriteLine("Using if-else...");
-            Console.WriteLine("Minimum number = {0}", minimum);
-            Console.WriteLine("Maximum number = {0}", maximum);
-            Console.ReadLine();*/
+            Console.WriteLine("element - {0} : ", i);
+            a[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        for (i = 0; i < n; i++)
+        {
+            sum += a[i];
+        }
+
+        Console.WriteLine("Sum: {0}\n\n", sum);
+        Console.ReadLine(); */
 
 
-            //q2
-            /*int num1, num2, num3;
-            Console.Write("Input the number 1:");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the number 2 :");
-            num2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the number 3:");
-            num3 = Convert.ToInt32(Console.ReadLine());
-                        if (num1 > num2)
-                        {
-                            if (num1 > num3)
-                            {
-                                Console.Write("The 1st Number is the greatest among three. \n\n");
-                            }
-                            else
-                            {
-                                Console.Write("The 3rd Number is the greatest among three. \n\n");
-                            }
-                        }
-                        else if (num2 > num3)
-                            Console.Write("The 2nd Number is the greatest among three \n\n");
-                        else
-                            Console.Write("The 3rd Number is the greatest among three \n\n");
-                        Console.ReadLine();*/
+        //q4
+
+        /*  ArrayList arr = new ArrayList { 5 };
+          arr.Add(90);
+          arr.Add(65);
+          arr.Add(76);
+          arr.Add(63);
+          arr.Add(54);
+          Console.WriteLine("Count {0}",arr.Count);
+          arr.Remove(76);
+          Console.WriteLine("Remove {0}",arr.Count);
+          Console.ReadLine(); */
+
+        //q5
+        /*int[][] classroom = new int[4][];
+          classroom[0] = new int[] { 11, 12, 13, 14 };
+          classroom[1] = new int[] { 101, 323 };
+          classroom[2] = new int[] { 89, 23, 897 };
+          classroom[3] = new int[] { 0, 45, 78, 53, 99 };
 
 
-            //q3
-            /* char ch1;
-             char ch2;
-             Console.Write("Enter a character: ");
-             ch1 = Console.ReadLine()[0];
-             Console.Write("Enter another character: ");
-             ch2 = Console.ReadLine()[0];
-             if (ch1 == ch2)
-                 Console.WriteLine("Input characters are the same");
-             else
-                 Console.WriteLine("Input characters are not the same");
-                 Console.ReadLine();*/
+          for (int n = 0; n < classroom.Length; n++)
+          {
 
 
-            //q4
-            /*string s1;
-            string s2;
+              System.Console.Write("student({0}): ", n);
 
-            Console.WriteLine("Enter the first string:");
-            s1 = Console.ReadLine();
-            Console.WriteLine("Enter the second string : ");
-            s2 = Console.ReadLine();
-
-            if (s1.Equals(s2))
-
-                Console.WriteLine("Both string are equal");
-            else
-                Console.WriteLine("string are not equal");
-                Console.ReadLine();*/
-
-            //q5
-
-            /* int num1, num2;
-             Console.WriteLine("Enter first number:");
-             num1 = Convert.ToInt32(Console.ReadLine());
-             Console.WriteLine("Enter second number:");
-             num2 = Convert.ToInt32(Console.ReadLine());
-             if (num1 > num2)
-                 Console.WriteLine("First number is bigger than second number", num1);
-             else
-                 Console.WriteLine("Second number is bigger than first number",num2);
-             Console.ReadLine();*/
-
-            //q6
-            /*int year;
-            Console.WriteLine("Enter Year : ");
-             year = int.Parse(Console.ReadLine());
-            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) Console.WriteLine("{0} is a Leap Year.", year);
-            else Console.WriteLine("{0} is not a Leap Year.", year);
-            Console.ReadLine();*/
-
-            //q7
-
-            /* int a, b, c;
-             Console.WriteLine("Enter the First subject mark:");
-             a = Convert.ToInt32(Console.ReadLine());
-             Console.WriteLine("enter the second subject mark:");
-             b = Convert.ToInt32(Console.ReadLine());
-             Console.WriteLine("Enter the Third subject mark:");
-             c = Convert.ToInt32(Console.ReadLine());
-             int Totalmark = a + b + c;
-             Console.WriteLine("total_marks=" + " " + Totalmark);
-             float per = Totalmark / 3;
-             if (per < 35)
-             {
-                 Console.WriteLine("fail");
-                 Console.ReadLine();
-             }
-             else if (per > 35 && per < 45)
-             {
-                 Console.WriteLine("Third ");
-                 Console.ReadLine();
-             }
-             else if (per > 45 && per < 60)
-             {
-                 Console.WriteLine("Second ");
-                 Console.ReadLine();
-             }
-             else
-             {
-                 Console.WriteLine("First");
-             }
-             Console.ReadLine(); */
+              for (int k = 0; k < classroom[n].Length; k++)
+              {
 
 
-            //q8
-            /* int rows;
-             Console.Write("Enter number of rows :");
-             rows = Convert.ToInt32(Console.ReadLine());
-             for (int i = 1; i <= rows; i++)
-             {
-               for (int j = 1; j <= i; j++)
-               {
-                Console.Write("*");
-                 }
-             Console.WriteLine("\n");
+                  System.Console.Write("{0} ", classroom[n][k]);
               }
-             Console.ReadLine(); */
-
-            //q9
-            /* int i, n, sum = 0;
-             Console.Write("Display the sum of n odd natural number:\n");
-             Console.Write("\n\n");
-
-             Console.Write("Input number of terms : ");
-             n = Convert.ToInt32(Console.ReadLine());
-             Console.Write("\nThe odd numbers are :");
-             for (i = 1; i <= n; i++)
-             {
-                 Console.Write("{0} ", 2 * i - 1);
-                 sum += 2 * i - 1;
-             }
-             Console.Write("\nThe Sum of odd Natural Number upto {0} terms : {1} \n", n, sum);
-             Console.ReadLine(); */
-
-            //q10
-
-            /* int num, r, sum = 0, s;
-            Console.Write("Reverse order:\n");
-            Console.Write("\n\n");
+              System.Console.WriteLine();
+          }
+        Console.ReadLine(); */
 
 
-            Console.Write("Input a number: ");
-            num = Convert.ToInt32(Console.ReadLine());
+        //q6
+        /* Stack a = new Stack();
+         a.Push(1);
+         a.Push(2);
+         a.Push(3);
+         Console.WriteLine("Before pop:");
+         foreach (Object obj in a)
+         {
+             Console.WriteLine(obj);
+         }
 
-            for (s = num; s != 0; s= s / 10)
+         Console.WriteLine("Before pop count is " + a.Count);
+         a.Pop();
+         Console.WriteLine("After pop:");
+         foreach (Object obj in a)
+         {
+             Console.WriteLine(obj);
+         }
+
+         Console.WriteLine("Peek  " + a.Peek());
+         Console.WriteLine("after pop count is " + a.Count);
+         Console.ReadLine(); */
+
+
+        //q7
+        /* Queue<int> s = new Queue<int>();
+         s.Enqueue(1);
+         s.Enqueue(11);
+         s.Enqueue(111);
+         s.Enqueue(1111);
+         s.Enqueue(11111);
+         foreach (var element in s)
+         {
+             Console.WriteLine(element);
+
+         }
+         Console.WriteLine("Total count :" + s.Count);
+         s.Dequeue();
+         Console.WriteLine("Count after performing Dequeue :" + s.Count);
+         Console.WriteLine("Top most element is: " + s.Peek());
+         Console.ReadLine(); */
+
+        //q8
+        /* Hashtable h = new Hashtable();
+         h.Add(1, "Hey");
+         h.Add(2, "Hearty");
+         h.Add(3, "Welcome!!");
+
+         Console.WriteLine("Before delete:");
+
+         foreach (DictionaryEntry element in h)
+         {
+             Console.WriteLine("{0} and {1} ", element.Key, element.Value);
+         }
+         h.Remove(1);
+         Console.WriteLine("After perform delete:");
+         foreach (DictionaryEntry element in h)
+         {
+             Console.WriteLine("{0} and {1} ", element.Key, element.Value);
+         }
+         Console.ReadLine(); */
+
+        //q9
+        /*float a = new float[] { 0.1f, 8.8f, 9.6f, 5.4f, 6.9f, 7.3f };
+       Array.Sort(a);
+       Console.WriteLine("Ascending Order: ");
+       foreach (float value in a)
+       {
+           Console.Write(value + " ");
+       }
+       Console.WriteLine("\n");
+       Array.Reverse(a);
+       Console.WriteLine("Dscending order : ");  /*float
+       foreach (float value in a)
+       {
+           Console.Write(value + " ");
+       }
+       Console.ReadLine(); */
+
+        //q10
+       /* static void Main(string[] args)
+        {
+            Dictionary<int, string> dct = new Dictionary<int, string>();
+
+            dct.Add(1, "cs.net");
+            dct.Add(2, "vb.net");
+            dct.Add(3, "vb.net");
+            dct.Add(4, "vb.net");
+            foreach (KeyValuePair<int, string> kvp in dct)
             {
-                r = s % 10;
-                sum = sum * 10 + r;
+                Console.WriteLine(kvp.Key + " " + kvp.Value);
             }
-            Console.Write("The number in reverse order is : {0} \n", sum);
-            Console.ReadLine();*/
-
-            //q11
-
-            /* int sum = 0;
-             for (int i = 100; i <= 200; i++)
-             {
-
-                 sum = sum + i;
-             }
-             Console.Write(sum);
-             Console.ReadLine(); */
-
-            //q12
-            /* int i, sum = 0;
-             Console.Write("Numbers between 100 and 200, divisible by 9 : \n");
-             for (i = 101; i < 200; i++)
-             {
-                 if (i % 9 == 0)
-                 {
-                     Console.Write("{0}  ", i);
-                     sum += i;
-                 }
-             }
-             Console.Write("\n\nThe sum : {0} \n", sum);
-             Console.ReadLine(); */
-
-
-            //q13
-
-             /* int num;
-              Console.WriteLine("Enter the number:");
-              num= Convert.ToInt32(Console.ReadLine());
-              if (num== 0)
-              {
-                  Console.WriteLine("IT IS ZERO");
-              }
-              else if (num > 100)
-              {
-                  Console.WriteLine("Too large");
-              }
-              else if (num / 10 == 0)
-              {
-                  Console.WriteLine("multiple of 10");
-              }
-              else if (num%2 == 0)
-              {
-                  Console.WriteLine("it is even");
-              }
-              else
-              {
-                  Console.WriteLine("it is odd");
-              }
-              Console.ReadLine(); */
-
-            //q14
-           /* int num;
-            Console.WriteLine("Enter the number:");
-            num= Convert.ToInt32(Console.ReadLine());
-            switch (num == 0)
+            dct.Remove(1);
+            Console.WriteLine("after removing");
+            foreach (KeyValuePair<int, string> kvp in dct)
             {
-                case true:
-                    Console.WriteLine("given inuput is zero");
-                    break;
-                case false:
-                    if (num % 2 == 0)
-                    {
-                        Console.WriteLine("given input is even");
-                    }
-                    else
-                    {
-                        Console.WriteLine("given input is odd");
-                    }
-                    break;
+                Console.WriteLine(kvp.Key + " " + kvp.Value);
             }
-            Console.ReadLine(); */
+        }  */
+        
 
-            //q15
 
-            /* int a, b;
-             for ( a = 0, b = 25; a <= 25 & b >= 0; a++, b--)
-             {
-                 if (a > b)
-                 {
-                     Console.WriteLine("It's crossed");
-                     break;
 
-                 }
-                 else
-                 {
-                     Console.Write(a);
-                     Console.WriteLine(b);
-                 }
-             }
-             Console.ReadLine(); */
+        //q11
+        /*SortedList<int, string> dct = new SortedList<int, string>();
+          dct.Add(1, "appnet");
+          dct.Add(2, "ab.net");
+          dct.Add(3, "pc.net");
+          dct.Add(4, "vb.net");
+          foreach (KeyValuePair<int, string> kvp in dct)
+          {
+              Console.WriteLine(kvp.Key + " " + kvp.Value);
+          }
+          dct.Remove(2);
+          Console.WriteLine("after removing");
+          foreach (KeyValuePair<int, string> kvp in dct)
+          {
+              Console.WriteLine(kvp.Key + " " + kvp.Value);
+          }
+          Console.ReadLine(); */
 
-            //q16
-            int score = 100,i;
-            string input;
-            Console.WriteLine("Enter given input hit or miss:");
-            for (i = 0; i <= 3; i++)
+
+        //q12
+        /* int[] arr = new int[3] {89,67,56};
+        int i, maximum, minimum, n;
+        n = 3;
+        maximum = arr[0];
+        minimum = arr[0];
+        for (i = 1; i < n; i++)
+        {
+            if (arr[i] > maximum)
             {
-                input = Console.ReadLine();
-                if (input == "hit")
+                maximum = arr[i];
+            }
+            if (arr[i] < minimum)
+            {
+                minimum = arr[i];
+            }
+        }
+        Console.WriteLine("Maximum element is {0}\n", maximum);
+        Console.WriteLine("Minimum element is {0}\n\n", minimum);
+        Console.ReadLine(); */
+
+        //q13
+        /*nt[] arr = new int[] { 1, 3, 5, 200, 22, 2244, 213, 111 };
+         int res = 100;
+         Console.WriteLine("Given Integer {0}: ", res);
+         Console.WriteLine("Numbers larger than {0} = ", res);
+         for (int i = 0; i < arr.Length; i++)
+         {
+             if (arr[i] > res)
+             {
+                 Console.WriteLine(arr[i]);
+                 Console.ReadLine();
+             } 
+           } */
+
+        //q14
+    /*    static int count(int[] S, int m, int n)
+       {
+           if (n == 0)
+               return 1;
+
+
+           if (n < 0)
+               return 0;
+
+           if (m <= 0)
+               return 0;
+
+
+           return count(S, m - 1, n) +
+               count(S, m, n - S[m - 1]);
+       }
+        public static void Main()
+        {
+            Console.Write("n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("m = ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("arr = ");
+            int[] arr = new int[n];
+            var stringArray = Console.ReadLine().Split(' ');
+            for (var i = 0; i < m; i++)
+            {
+                arr[i] = int.Parse(stringArray[i]);
+            }
+            Console.WriteLine("Output = {0}", count(arr, m, n));
+            Console.ReadLine(); 
+        } */
+
+        //q15
+      /*  public static void printFirstRepeating(int[] arr)
+        {
+            int minimum = -1;
+            HashSet<int> set = new HashSet<int>(); //empty hashset
+            for (int i = arr.Length - 1; i >= 0; i--) //traversing i/p array from rgt to lft
+            {
+                if (set.Contains(arr[i]))      //if element is already in hashset update min
                 {
-                    score = score + 10;
+                    minimum = i;
                 }
-                else if (input == "miss")
+                else
                 {
-                    score = score - 20;
+                    set.Add(arr[i]); // or add element to hash set
                 }
             }
-            Console.WriteLine(score);
+            if (minimum != -1)
+            {
+                Console.WriteLine("The first repeating element is " + arr[minimum]);
+            }
+            else
+            {
+                Console.WriteLine("No repeating elements");
+            }
             Console.ReadLine();
-
-
-
-
-
-
-
-
+        }
+             public static void Main(string[] args)
+            {
+                int[] arr = new int[] {1,5,3,4,3,5,6 };
+                printFirstRepeating(arr);
+            }
+            */
         }
     }
-}
+
