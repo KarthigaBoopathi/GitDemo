588f9ca5bb94afc1024d712c07a7403da21017be
Karthiga <karthigaboopathi2000@gmail.com>
2022-09-07 08:48:36 +0530

    assignment

---
+++ day15 06-09/access_specifiers/Class1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace access_specifiers
+{
+
+   /* public abstract class person
+    {
+
+        public abstract void work();
+
+    }
+    public class employees : person
+    {
+
+
+        public override void work()
+        {
+            Console.WriteLine("Emplpoyee");
+        }
+    }
+    public class manager : person
+    {
+
+
+        public override void work()
+        {
+            Console.WriteLine("Manager");
+        }
+    }
+    public class clerk : person
+    {
+
+
+        public override void work()
+        {
+            Console.WriteLine("Clerk");
+        }
+    }
+
+    //q2
+
+    public interface IBank
+    {
+
+        void print_balance();
+        void calc_interest();
+    }
+    public class bank : IBank
+    {
+
+        public double amount;
+
+        public bank()
+        {
+
+            amount = 0;
+        }
+
+        public void print_balance()
+        {
+            Console.WriteLine("Enter amount");
+            amount = Convert.ToDouble(Console.ReadLine());
+        }
+        public void calc_interest()
+        {
+            int i;
+            Console.WriteLine("enter the interest to calculate");
+            i = Convert.ToInt32(Console.ReadLine());
+            double result = amount * i;
+            Console.WriteLine("calc_interest " + result);
+            Console.ReadLine();
+        }
+    }
+
+    //q3
+
+    public interface banks
+    {
+
+        void print_balance();
+        void calc_interest();
+    }
+    public class savings : banks
+    {
+
+        public double amount;
+
+        public savings()
+        {
+
+            amount = 0;
+        }
+
+        public void print_balance()
+        {
+            Console.WriteLine("Enter amount");
+            amount = Convert.ToDouble(Console.ReadLine());
+        }
+        public void calc_interest()
+        {
+            float r = 0.18f;
+
+            double result = amount * r;
+            Console.WriteLine("calc_interest " + result);
+        }
+    }
+    public class current : banks
+    {
+
+        public double amount;
+
+        public current()
+        {
+
+            amount = 0;
+        }
+
+        public void print_balance()
+        {
+            Console.WriteLine("Enter amount");
+            amount = Convert.ToDouble(Console.ReadLine());
+        }
+        public void calc_interest()
+        {
+            float r = 0.01f;
+
+            double result = amount * r;
+            Console.WriteLine("calc_interest " + result);
+        }
+    } */
+
+    //q5
+    public class banks
+    {
+        public int x, y;
+        public banks()
+        {
+            x = 8000;
+            y = 98000;
+        }
+
+
+    }
+    public static class curr
+    {
+
+        public static void current(this banks obj)
+        {
+            Console.WriteLine("Current balance: " + obj.x);
+        }
+        public static void deposits(this banks obj)
+        {
+            Console.WriteLine("Enter the amount to be deposited in current account");
+            int dep = Convert.ToInt32(Console.ReadLine());
+            obj.x = dep + obj.x;
+            Console.WriteLine("Amount deposited successfully!!");
+        }
+        public static void withdraw(this banks obj)
+        {
+            Console.WriteLine("Enter the amount to be withdrawed in current account");
+            int with = Convert.ToInt32(Console.ReadLine());
+            if (with < obj.x)
+                obj.x = obj.x - with;
+            else
+            Console.WriteLine("Amount can't be withdrawn!!");
+            Console.WriteLine("Amount withdrawn successfully!!");
+
+        }
+        public static void total_balc(this banks obj)
+        {
+            Console.WriteLine("Total balance in current account: " + obj.x);
+        }
+    }
+    public static class sav
+    {
+
+        public static void savings(this banks obj)
+        {
+            Console.WriteLine("saving balance" + obj.y);
+        }
+        public static void deposits1(this banks obj)
+        {
+            Console.WriteLine("Enter the amount to be deposited in savings account:");
+            int dep = Convert.ToInt32(Console.ReadLine());
+
+            obj.y = dep + obj.y;
+            Console.WriteLine("Amount Deposited successfully!!");
+        }
+        public static void withdraw1(this banks obj)
+        {
+            Console.WriteLine("Enter the amount to withdraw in savings account:");
+            int with = Convert.ToInt32(Console.ReadLine());
+            if (with < obj.y)
+                obj.y = obj.y - with;
+            else
+            Console.WriteLine("Amount can't be withdrawn!");
+            Console.WriteLine("Amount withdrawn successfully!!");
+        }
+        public static void total_balc1(this banks obj)
+        {
+            Console.WriteLine("Total Balance in current account :" + obj.y);
+        }
+    }
+    }
+
+    
+
+
+
---
+++ day15 06-09/ass 06-09/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using access_specifiers;
+
+namespace ass_06_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //q1
+            /*  person p;
+               p = new employees();
+               p.work();
+               p = new manager();
+               p.work();
+               p = new clerk();
+               p.work(); 
+
+               //q2
+
+               bank b1 = new bank();
+               b1.print_balance();
+               b1.calc_interest(); 
+
+               //q3,4
+
+               Console.WriteLine("savings account");
+               banks b = new savings();
+               b.print_balance();
+               b.calc_interest();
+               Console.WriteLine("current aacount");
+               banks b2 = new current();
+               b2.print_balance();
+               b2.calc_interest(); */
+
+            //q5,6
+            Console.WriteLine("Amount Depsoits and Withdrawl------");
+            banks b3 = new banks();
+            b3.current();
+            b3.savings();
+            b3.deposits();
+            b3.withdraw();
+            b3.total_balc();
+            b3.deposits1();
+            b3.withdraw1();
+            b3.total_balc1();
+            Console.ReadLine();
+            
+
+           
+            
+
+        }
+    }
+
+    } 
+    
+
+
+            
+        
+    
+
+
