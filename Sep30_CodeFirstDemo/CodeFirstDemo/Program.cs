7a7e9efd4b5b9050c59635bd87fe0e29d0be67a2
Karthiga <karthigaboopathi2000@gmail.com>
2022-10-12 22:16:04 +0530

    assignments

---
+++ Sep30_CodeFirstDemo/CodeFirstDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstDAL;
+
+namespace CodeFirstDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyContext context = new MyContext();
+            context.StudentsTable.Add(new Student { Rollno = 1, Studname = "Karthiga", City = "Chennai", DOB = new DateTime(2022, 12, 11) });
+            context.SaveChanges();
+            Console.WriteLine("done");
+            Console.Read();
+        }
+    }
+}
