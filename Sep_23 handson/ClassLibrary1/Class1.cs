2810c80bc742068b419592172914bb5fa35e12c1
Karthiga <karthigaboopathi2000@gmail.com>
2022-10-12 21:19:01 +0530

    assignments

---
+++ Sep_23 handson/ClassLibrary1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private int productid;
+        public int ID
+        {
+            get { return productid; }
+            set { if (productid == null)
+                    throw new Exception("Please Enter correct value");
+                else { productid = value; }
+                        }
+        }
+
+        private string productname;
+        public string Name
+        {
+            get { return productname; }
+            set
+            {
+                if((String.IsNullOrEmpty(value)))
+                {
+                    throw new Exception("Please Enter Correct value");
+                }
+                else
+                {
+                    productname = value;
+                }
+            }
+        }
+        private int unitprice;
+        public int Price
+        {
+            get { return unitprice; }
+            set
+            {
+                if(unitprice==null)
+                {
+                    throw new Exception("Please Enter Correct value");
+                }
+                else
+                {
+                    unitprice = value;
+                }
+            }
+        }
+    }
+}
---
+++ Sep_23 handson/Sep_23 handson/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace Sep_23_handson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = null;
+            Class1 obj = new Class1();
+            //  Console.WriteLine("enter the product key to search");
+            // obj.ID=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the amount to search");
+            obj.Price = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection("Data source=LAPTOP-H55UBBSP\\SQLEXPRESS; Database=ProductsTable; integrated security=SSPI");
+                // writing sql query  
+                SqlCommand cm = new SqlCommand("select * from Products where unitprice='" + obj.Price + "'", con);
+                // Opening Connection  
+                con.Open();
+                // Executing the SQL query  
+                SqlDataReader sdr = cm.ExecuteReader();
+                while (sdr.Read())
+                {
+                    Console.WriteLine(sdr["productid"] + " " + sdr["productname"] + " " + sdr["unitprice"]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPS, Something went wrong." + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
